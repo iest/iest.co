3596e9d0f7e679a6a719d4ae90559ce7c09b2c2d
Iestyn Williams <iest@me.com>
2016-04-29 14:45:46 +0000

    Fancy nav button working

--- components/nav.js
+++ components/nav.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import {Motion, spring} from 'react-motion';
 import {Link} from 'react-router';
 import {InlineBlock, Block, Flex, Col} from 'jsxstyle';
 
 import {primary, bg, secondary} from 'utils/theme';
 import Icn from 'components/icn';
 import Btn from 'components/btn';
+
+const immediate = {
+  stiffness: 180,
+  damping: 18,
+};
+const fast = {
+  stiffness: 270,
+  damping: 22,
+};
 
 export default class Nav extends Component {
   state = {
@@ -36,18 +46,42 @@ export default class Nav extends Component {
             padding="0em"
             props={{onClick: this.toggleOpen}}
           >
-            <Col
-              width="1em"
-              height="0.75em"
-              justifyContent="space-between"
+            <Motion
+              style={{
+                r1: spring(open ? 225 : 0, immediate),
+                r2: spring(open ? 135 : 0, immediate),
+                y: spring(open ? 0.34 : 0, fast),
+                o: spring(open ? 0 : 1, immediate),
+              }}
             >
-              <Block height="1px" background={secondary} />
-              <Block height="1px" background={secondary} />
-              <Block height="1px" background={secondary} />
-            </Col>
+              {({r1, r2, y, o}) =>
+                <Col
+                  width="1em"
+                  height="0.75em"
+                  justifyContent="space-between"
+                  position="relative"
+                >
+                  <Block
+                    transform={`translateY(${y}em) rotate(${r1}deg)`}
+                    height="0.0625em"
+                    background={secondary}
+                  />
+                  <Block
+                    opacity={o}
+                    transform={`rotate(${r2}deg)`}
+                    height="0.0625em"
+                    background={secondary}
+                  />
+                  <Block
+                    transform={`translateY(${-y}em) rotate(${r2}deg)`}
+                    height="0.0625em"
+                    background={secondary}
+                  />
+                </Col>
+              }
+            </Motion>
           </Btn>
         </Flex>
-        {open ? 'O' : 'I'}
       </nav>
     );
   }
